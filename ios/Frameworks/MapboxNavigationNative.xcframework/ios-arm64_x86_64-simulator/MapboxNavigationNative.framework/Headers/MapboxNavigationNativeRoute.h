71459cc5ae29b1417cc5419e3379e96e8be3cc29
fbeccaceci <0123fabri@gmail.com>
2024-10-21 13:33:24 +0200

    Setup git lfs

---
+++ ios/Frameworks/MapboxNavigationNative.xcframework/ios-arm64_x86_64-simulator/MapboxNavigationNative.framework/Headers/MapboxNavigationNativeRoute.h
@@ -0,0 +1,15 @@
+// This file is generated and will be overwritten automatically.
+
+#import "MBNNMapboxAPI.h"
+#import "MBNNWaypointType.h"
+#import "MBNNUpcomingRouteAlert.h"
+#import "MBNNUpcomingRouteAlertUpdate.h"
+#import "MBNNRouteInfo.h"
+#import "MBNNRouteIndices.h"
+#import "MBNNWaypoint.h"
+#import "MBNNRouteIntersection.h"
+#import "MBNNAlternativeRouteInfo.h"
+#import "MBNNRouteAlternative.h"
+#import "MBNNRouteParser.h"
+#import "MBNNRouteInterface.h"
+#import "MBNNRoutesData.h"
